30954fefaa760496e237c3e909f00693a340c78d
solomadm <solomadm78@gmail.com>
2018-03-22 11:12:15 +0200

    some styles fixes

--- src/app/components/main-slider/main-slider.component.ts
+++ src/app/components/main-slider/main-slider.component.ts
@@ -3,27 +3,96 @@ import { ChangeDetectionStrategy, Input } from '@angular/core';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 type PaneType = 'trampoline' | 'escape' | 'virtual';
 @Component({
-    selector: 'app-main-slider',
-    templateUrl: './main-slider.component.html',
-    styleUrls: ['./main-slider.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [
-        trigger('slide', [
-            state('trampoline', style({ transform: 'translateX(0)' })),
-            state('escape', style({ transform: 'translateX(-33.33%)' })),
-            state('virtual', style({ transform: 'translateX(-66.66%)' })),
-            transition('* => *', [animate('1.0s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
-              animate('1.0s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
-                animate('1.0s 3.1s ease-in', style({transform: 'translateX(0%)'}))
-
-        ])
-])]
+  selector: 'app-main-slider',
+  templateUrl: './main-slider.component.html',
+  styleUrls: ['./main-slider.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('slide', [
+      state('trampoline', style({ transform: 'translateX(0)' })),
+      state('escape', style({ transform: 'translateX(-33.33%)' })),
+      state('virtual', style({ transform: 'translateX(-66.66%)' })),
+      transition('* <=> *', [
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-33.33%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(-66.66%)'})),
+        animate('0.01s 3.1s ease-in', style({transform: 'translateX(0)'})),
+      ])
+    ])]
 })
 export class MainSliderComponent implements OnInit {
-    @Input () activePane: PaneType = 'trampoline';
-    constructor() { }
+  @Input () activePane: PaneType = 'trampoline';
+  constructor() { }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
 }
--- src/app/services/init-js.service.ts
+++ src/app/services/init-js.service.ts
@@ -59,7 +59,7 @@ export class InitJsService {
                         };
                     }
 
-                
+
             })();
         }, 0);
     }
