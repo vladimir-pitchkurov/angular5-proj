d83b4004341d7763ac50627507987bfdfd75c881
solomadm <solomadm78@gmail.com>
2018-04-03 10:01:05 +0300

    changed the menu item trampoline-park

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {EscapeRoomComponent} from './components/escape-room/escape-room.component';
-import {BlogComponent} from './components/blog/blog.component';
-import {BlogPostComponent} from './components/blog-post/blog-post.component';
-import {TrampolinePartiesComponent} from './components/trampoline-parties/trampoline-parties.component';
-import {ComingSoonComponent} from './components/locations/coming-soon/coming-soon.component';
-import {HomeComponent} from './components/locations/home/home.component';
-import {JoinourteamComponent} from './components/locations/joinourteam/joinourteam.component';
-import {GalleryComponent} from './components/locations/gallery/gallery.component';
-import {RulesComponent} from './components/locations/rules/rules.component';
-import {AboutComponent} from './components/locations/about/about.component';
-import {ContactComponent} from './components/locations/contact/contact.component';
-import {ActivitiesComponent} from './components/locations/activities/activities.component';
-import {WaiverComponent} from './components/locations/waiver/waiver.component';
-import {GroupsComponent} from './components/locations/groups/groups.component';
-import {TrampolinesHomepageComponent} from './components/locations/trampolines-homepage/trampolines-homepage.component';
-import {PassComponent} from './components/locations/pass/pass.component';
-import {PartiesComponent} from './components/locations/parties/parties.component';
-import {WelcomeComponent} from './components/welcome/welcome.component';
-import {TrampolineHomeComponent} from './components/locations/trampoline-home/trampoline-home.component';
+import { EscapeRoomComponent } from './components/escape-room/escape-room.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { TrampolinePartiesComponent } from './components/trampoline-parties/trampoline-parties.component';
+import { ComingSoonComponent } from './components/locations/coming-soon/coming-soon.component';
+import { HomeComponent } from './components/locations/home/home.component';
+import { JoinourteamComponent } from './components/locations/joinourteam/joinourteam.component';
+import { GalleryComponent } from './components/locations/gallery/gallery.component';
+import { RulesComponent } from './components/locations/rules/rules.component';
+import { AboutComponent } from './components/locations/about/about.component';
+import { ContactComponent } from './components/locations/contact/contact.component';
+import { ActivitiesComponent } from './components/locations/activities/activities.component';
+import { WaiverComponent } from './components/locations/waiver/waiver.component';
+import { GroupsComponent } from './components/locations/groups/groups.component';
+import { TrampolinesHomepageComponent } from './components/locations/trampolines-homepage/trampolines-homepage.component';
+import { PassComponent } from './components/locations/pass/pass.component';
+import { PartiesComponent } from './components/locations/parties/parties.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TrampolineHomeComponent } from './components/locations/trampoline-home/trampoline-home.component';
+import { TrampolineParkComponent } from './components/trampoline-park/trampoline-park.component';
 
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'trampoline-park', component: TrampolineHomeComponent},
+  { path: 'trampoline-park', component: TrampolineParkComponent },
   { path: 'trampoline-parties', component: TrampolinePartiesComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/:post', component: BlogPostComponent },
